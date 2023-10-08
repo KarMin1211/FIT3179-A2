3263b8ab2a3b987a18950ad128732407153ff196
KarMinTan <ktan0115@student.monash.edu>
2023-10-08 21:43:24 +1100

    initial commit

---
+++ map.js
@@ -0,0 +1,29 @@
+var vg_1 = "map.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "bubbleplot.vg.json";
+vegaEmbed("#bubbleplot", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "barchart.vg.json";
+vegaEmbed("#barchart", vg_3).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "boxplot.vg.json";
+vegaEmbed("#boxplot", vg_4).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "ranged dot plot.vg.json";
+vegaEmbed("#rangedplot", vg_5).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "lollipop.vg.json";
+vegaEmbed("#lollipop", vg_6).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
