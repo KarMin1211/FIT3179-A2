d9038b25f0d3cb01c073c95a7eb15a48f372f79f
KarMinTan <ktan0115@student.monash.edu>
2023-10-15 20:16:45 +1100

    second commit

--- embedding.js
+++ embedding.js
@@ -1,29 +1,34 @@
 var vg_1 = "map.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "bubbleplot.vg.json";
-vegaEmbed("#bubbleplot", vg_2).then(function(result) {
+vegaEmbed("#bubbleplot", vg_2, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "barchart.vg.json";
-vegaEmbed("#barchart", vg_3).then(function(result) {
+vegaEmbed("#barchart", vg_3, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_4 = "boxplot.vg.json";
-vegaEmbed("#boxplot", vg_4).then(function(result) {
+vegaEmbed("#boxplot", vg_4, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_5 = "ranged dot plot.vg.json";
-vegaEmbed("#rangedplot", vg_5).then(function(result) {
+vegaEmbed("#rangedplot", vg_5, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_6 = "lollipop.vg.json";
-vegaEmbed("#lollipop", vg_6).then(function(result) {
+vegaEmbed("#lollipop", vg_6, {"actions": false}).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "mapusers.vg.json";
+vegaEmbed("#mapusers", vg_7, {"actions": false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
